b1ebdfb1728e6414bcd8bf55198cc26f5630961d
Raghu Betina <raghu@firstdraft.com>
2017-08-17 01:08:48 +0000

    Direct association between firms and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,8 @@
 class Bookmark < ApplicationRecord
   # Direct associations
 
+  belongs_to :firm
+
   belongs_to :user
 
   # Indirect associations
--- app/models/firm.rb
+++ app/models/firm.rb
@@ -1,6 +1,10 @@
 class Firm < ApplicationRecord
   # Direct associations
 
+  has_many   :visits,
+             :class_name => "Bookmark",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
