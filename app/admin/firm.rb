e4f5d9252ceaddf0f6056205768f70a6016240ec
Raghu Betina <raghu@firstdraft.com>
2017-08-17 01:09:09 +0000

    Whitelist columns in Firm for ActiveAdmin

--- app/admin/firm.rb
+++ app/admin/firm.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Firm do
 
+ permit_params :name, :mappable_address, :address, :category, :city, :state
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
